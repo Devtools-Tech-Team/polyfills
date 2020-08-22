9b7d7a4b889491e97715ac1d7f3862efcc19da32
Yomesh Gupta <yomesh.gupta@gmail.com>
2020-08-22 13:16:13 +0530

    Added reduce polyfill and test cases

---
+++ reduce/reduce.js
@@ -0,0 +1,33 @@
+function reduce(callback, initialValue) {
+	if (this === null) {
+		throw new TypeError('Array.prototype.reduce called on null or undefined');
+	}
+
+	if (!callback || typeof callback !== 'function') {
+		throw new TypeError(`${callback} is not a function`);
+	}
+
+	if (!this.length && initialValue) {
+		return initialValue;
+	} else if (!this.length && arguments.length < 2) {
+		throw new TypeError('Reduce of empty array with no initial value');
+	}
+
+	var k = 0;
+	var value = initialValue;
+
+	if (arguments.length < 2) {
+		value = this[k++];
+	}
+
+	while (k < this.length) {
+		if (Object.prototype.hasOwnProperty.call(this, k)) {
+			value = callback(value, this[k], k, this);
+		}
+		k++;
+	}
+
+	return value;
+}
+
+module.exports = reduce;

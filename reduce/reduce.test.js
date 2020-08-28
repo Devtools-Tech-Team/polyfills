676301456446ac9bea3e5b873b14ac3ca17e1c4e
Yomesh Gupta <yomesh.gupta@gmail.com>
2020-08-28 22:12:50 +0530

    Uncommented test cases

--- reduce/reduce.test.js
+++ reduce/reduce.test.js
@@ -16,14 +16,14 @@ describe('Error Conditions', () => {
 	});
 
 	// 2
-	// test('Check if callback is a function', () => {
-	// 	expect(() => [].customReduce()).toThrow(TypeError);
-	// });
+	test('Check if callback is a function', () => {
+		expect(() => [].customReduce()).toThrow(TypeError);
+	});
 
 	// 3
-	// test('Reduce on empty array with no initialValue', () => {
-	// 	expect(() => [].customReduce(sum)).toThrow(TypeError);
-	// });
+	test('Reduce on empty array with no initialValue', () => {
+		expect(() => [].customReduce(sum)).toThrow(TypeError);
+	});
 });
 
 describe('Reduce Functionality', () => {
@@ -32,43 +32,46 @@ describe('Reduce Functionality', () => {
 	});
 
 	// 4
-	// test('Invoked on empty array with initialValue, returns initialValue', () => {
-	// 	var initialValue = 5;
-	// 	expect([].customReduce(sum, initialValue)).toBe(initialValue);
-	// });
+	test('Invoked on empty array with initialValue, returns initialValue', () => {
+		var initialValue = 5;
+		expect([].customReduce(sum, initialValue)).toBe(initialValue);
+	});
 
 	// 5
-	// test('Invoked without initialValue', () => {
-	// 	var caller = [1, 2, 3];
+	test('Invoked without initialValue', () => {
+		var caller = [1, 2, 3];
 
-	// 	expect(caller.customReduce(sum)).toBe(6);
-	// });
+		expect(caller.customReduce(sum)).toBe(6);
+	});
 
 	// 6
-	// test('Invoked with initialValue', () => {
-	// 	var caller = [1, 2, 3];
+	test('Invoked with initialValue', () => {
+		var caller = [1, 2, 3];
 
-	// 	expect(caller.customReduce(sum, 1)).toBe(7);
-	// });
+		expect(caller.customReduce(sum, 1)).toBe(7);
+	});
 
 	// 7
-	// test('Custom Test Case | Reduce Promise', () => {
-	// 	function first() {
-	// 		return Promise.resolve(1);
-	// 	}
-	// 	function second(v) {
-	// 		return Promise.resolve(v + 2);
-	// 	}
-	// 	function third(v) {
-	// 		return Promise.resolve(v + 3);
-	// 	}
-	// 	function fourth(v) {
-	// 		return Promise.resolve(v + 4);
-	// 	}
+	test('Custom Test Case | Reduce Promise', () => {
+		function first() {
+			return Promise.resolve(1);
+		}
+		function second(v) {
+			return Promise.resolve(v + 2);
+		}
+		function third(v) {
+			return Promise.resolve(v + 3);
+		}
+		function fourth(v) {
+			return Promise.resolve(v + 4);
+		}
 
-	// 	var promises = [first, second, third, fourth];
-	// 	var answer = promises.reduce((acc, current) => acc.then(current), Promise.resolve());
+		var promises = [first, second, third, fourth];
+		var answer = promises.customReduce(
+			(acc, current) => acc.then(current),
+			Promise.resolve()
+		);
 
-	// 	expect(answer).resolves.toEqual(10);
-	// });
+		expect(answer).resolves.toEqual(10);
+	});
 });
